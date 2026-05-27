929ecc79806876dd3b179251f17179e573bc27fb
twosorry <blackbeer401@gmail.com>
2026-05-27 17:31:40 +0900

    day3fix

---
+++ Day03/ex1.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type:text/html; charset=utf-8");
+    $user_name = $_GET["user_name"];
+    $user_tel = $_GET["user_tel"];
+    $user_email = $_GET["user_email"];
+    echo("예약 완료");
+
+?>
---
+++ Day03/ex2.php
@@ -0,0 +1,9 @@
+<?php
+
+header("Content-Type:text/html; charset=utf-8");
+    $user_name = $_POST["user_name"];
+    $user_tel = $_POST["user_tel"];
+    $apply_field = $_POST["apply_field"];
+    $user_text = $_POST["user_text"];
+    echo("지원이 완료되었습니다.");
+?>
