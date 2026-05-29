1ab72282398a23419a918d390944e9323c64b1c5
twosorry <blackbeer401@gmail.com>
2026-05-29 17:31:13 +0900

    DAY5

--- Day04/ex/ccc.php
+++ Day04/ex/ccc.php
@@ -8,7 +8,7 @@
 
     $db = mysqli_connect('localhost','monster2026aix','a1s2d3f4!','monster2026aix');
     mysqli_query($db ,'set names utf8');
-    $sql = "INSERT INTO ex02(name, tel, apply_field, reason) VALUE('$name', '$tel', '$apply_field','$reason')";
+    $sql = "INSERT INTO ex02(name, tel, apply_field, reason) VALUES('$name', '$tel', '$apply_field','$reason')";
     $result = mysqli_query($db, $sql);
     if ($result){
         echo "예약되었습니다.";
@@ -17,7 +17,7 @@
         echo "에약에 실패하였습니다.";
 
     }
-    mysqli_close($db)
+    mysqli_close($db);
 
 
 ?>
--- Day04/ex/ddd.php
+++ Day04/ex/ddd.php
@@ -2,23 +2,27 @@
 
     header("Content-Type:text/html; charset=utf-8");
 
-    $db = mysqli_connect('localhost','monster2026','a1s2d3f41','monster2026aix');
+    $db = mysqli_connect('localhost','monster2026aix','a1s2d3f4!','monster2026aix');
     mysqli_query($db, 'set names utf8');
     $sql = 'SELECT * FROM ex02';
-    $result_table = mysqli_query($db, $sql)
+    $result_table = mysqli_query($db, $sql);
     if($result_table){
         $row_num = mysqli_num_rows($result_table);
-        for($i=0,$i<$row_num,$i++);
-        $row = mysqli_fetch_array($result_table,MYSQLI_ASSOC);
-        $no = $row["no"];
-        $tel = $row["tel"];
-        $apply_field = $row["apply_field"];
-        $reason = $row["reason"];
+        for($i=0;$i<$row_num; $i++){
+            $row = mysqli_fetch_array($result_table,MYSQLI_ASSOC);
+            $name = $row['name'];
+            $no = $row["no"];
+            $tel = $row["tel"];
+            $apply_field = $row["apply_field"];
+            $reason = $row["reason"];
 
-        echo "$no<p>";
-        echo "$tel<p>";
-        echo "$apply_field<p>";
-        echo "$reason<p>";
+            echo "$no<br>";
+            echo "$name<br>";    
+            echo "$tel<br>";
+            echo "$apply_field<br>";
+            echo "$reason<br>";
+            echo "<hr>";
+        }
     }else{
         echo "접수에 실패하였습니다.";
     }
