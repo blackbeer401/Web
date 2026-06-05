47ee1fbd8feb57cafe114343c163e132fbdb87d7
twosorry <blackbeer401@gmail.com>
2026-06-05 17:56:19 +0900

    day9

---
+++ Day09/01_ex.js
@@ -0,0 +1,48 @@
+// 브라우저의 흰색 화면에 글씨를 표기
+// 이 흰색 영역을 담당하는 js의 내장객체 document
+
+// 화면에 문자열 출력
+document.write("Hello World");
+// 다음줄 출력을 또하면 줄바꿈이 될까?
+document.write("줄바꿈 되나?");
+// 줄바꿈 안됨 그냥하면.. 줄바꿈 문자 <br>써야함;
+document.write("줄바꿈됨 <br>태그 이용");
+document.write("<hr>");
+document.write('<a href="https://www.naver.com">네이버</a>');
+
+document.write('<hr>');
+
+// 숫자 데이터 출력
+document.write(10);
+document.write('<br>')
+document.write(3+5);
+document.write('<br>')
+// 문자열 데이터의 덧셈은 산술연산이 아니라 결합
+
+document.write('aa'+'bb')
+document.write('<br>');
+// 숫자 + 문자열
+document.write(10+'aaa') ; // 파이썬에서는 에러나지만 js에서는 결합연산
+document.write('<hr>')
+// js 는 프로그래밍 언어이기에 변수 및 객체, 제어문, 연산자 등이 존재함
+var a=10;
+document.write(a)
+document.write('a')
+document.write('<br>')
+
+var b= new Date(); // 날짜와 시간 정보를 관리하는 객체를 생성
+document.write(b);
+document.write('<br>')
+
+for(var i=0; i<10 ; i ++){
+    document.write( i + ",")
+}
+document.write('<br>')
+
+// 버튼 클릭 같은 사용자의 이벤트에 반응하는 기능함수
+function aaa(){
+    alert('clicked button'); //경고창을 보여주는 js 의 내장함수
+}
+
+// 함수는 호출해야만 그 안에 있는 코드가 실행됨
+//aaa();
